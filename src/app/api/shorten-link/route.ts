b7d3c235f6f36567f854414d62f45b3a2921ac4e
Franco Saavedra Cornejo <francosaavedra@hotmail.com>
2024-10-08 13:17:37 -0500

    adding console.logs to test for errors

--- src/app/api/shorten-link/route.ts
+++ src/app/api/shorten-link/route.ts
@@ -1,14 +1,18 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
-  try {    
+  try {
+    console.log('Request received at /api/shorten-link');
     
-    const { url } = await request.json();    
-    
+    const { url } = await request.json();
+    console.log('URL received:', url);
+
     const apiKey = process.env.URLDAY_API_KEY;
-    if (!apiKey) {      
+    console.log('API Key:', apiKey); // Log para verificar si se está obteniendo la API key en producción
+
+    if (!apiKey) {
       return NextResponse.json({ error: 'API Key is missing' }, { status: 500 });
-    }    
+    }
 
     const apiResponse = await fetch(`https://urlbae.com/api/url/add`, {
       method: 'POST',
@@ -17,15 +21,20 @@ export async function POST(request: Request) {
         'Authorization': `Bearer ${apiKey}`
       },
       body: JSON.stringify({
-        url: url 
+        url: url
       })
-    });  
+    });
 
-    if (apiResponse.status != 200) {     
+    console.log('Response status from URL Bae:', apiResponse.status);
+
+    if (apiResponse.status !== 200) {
+      const errorText = await apiResponse.text();
+      console.error('Error from URL shortening API:', apiResponse.status, errorText);
       return NextResponse.json({ error: apiResponse.statusText }, { status: apiResponse.status });
     }
 
-    const data = await apiResponse.json();    
+    const data = await apiResponse.json();
+    console.log('Data received from URL Bae:', data);
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
     console.error('Error in API route:', error);
